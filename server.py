966bd91e0045dcf032808bdf127407143fe1d8e8
madeline <madeline.nivar@gmail.com>
2024-02-25 23:22:44 -0500

    progress

--- server.py
+++ server.py
@@ -3,26 +3,27 @@
     localhost:5000.
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
+
+from flask import Flask, request, render_template
+             
 # Import the sentiment_analyzer function from the package created: TODO
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
 #Initiate the flask app : TODO
+app = Flask(__name__)
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
-    ''' This code receives the text from the HTML interface and 
-        runs sentiment analysis over it using sentiment_analysis()
-        function. The output returned shows the label and its confidence 
-        score for the provided text.
-    '''
-    # TODO
+    text_to_analyze = request.form["textToAnalyze"]
+    result = sent_analyzer(text_to_analyze)
+
+    # return render index with the result this would be for a POST?
+
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
-    #TODO
+    return render_template("index.html")
+    
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+   app.run(debug=True)
