7af56c3b6bf0e3c808e10b11986942957c8bf519
madeline <madeline.nivar@gmail.com>
2024-03-02 16:11:51 -0500

    idk

--- server.py
+++ server.py
@@ -3,27 +3,36 @@
     localhost:5000.
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
-
 from flask import Flask, request, render_template
-             
 # Import the sentiment_analyzer function from the package created: TODO
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
-
 #Initiate the flask app : TODO
 app = Flask(__name__)
-
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
-    text_to_analyze = request.form["textToAnalyze"]
-    result = sent_analyzer(text_to_analyze)
+    """
+    Analyzes the sentiment of the given text.
 
-    # return render index with the result this would be for a POST?
+    Parameters:
+    text_to_analyse (str): Text to analyze.
 
-
+    Returns:
+    dict: A dictionary containing the sentiment label and score.
+    """
+    text_to_analyze = request.args.get("textToAnalyze")
+    response = sentiment_analyzer(text_to_analyze)
+    label = response['label']
+    score = response['score']
+    if label is None:
+        return "Invalid input ! Try again."
+    return f"The given text has been identified as {label.split('_')[1]} with a score of {score}."
 @app.route("/")
 def render_index_page():
-    return render_template("index.html")
-    
+    """
 
+    renders page
+
+    """
+    return render_template("index.html")
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(debug=True)
